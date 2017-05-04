eee21eb8c228e1c2c8be0e847596742022aade17
Sirithang <saby.guillaume@gmail.com>
2017-05-04 16:08:29 +0100

    couple more instructions + revamp memory to be a single block instead of split between (future) devices

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -3,9 +3,10 @@
 
 using namespace cpu;
 
-#define BITTEST(value,bit) (value >> bit) & 1
+#define BITTEST(value,bit) ((value) & (1<<(bit)))
 #define BITSET(value,bit) value |= 1 << bit
 #define BITCLEAR(value,bit) value &= ~(1 << bit)
+#define HALF_CARRY_TEST(a,b) ((a&0xf) + (b&0xf))&0x10
 
 void bitTest(CPU& cpu, u8 value, u8 bit)
 {
@@ -17,7 +18,7 @@ void bitTest(CPU& cpu, u8 value, u8 bit)
 
 int extendedDecode(CPU& cpu, u8 opcode)
 {
-	printf("Decoding extended %hhX\n", opcode);
+	//printf("Decoding extended %hhX\n", opcode);
 
 	switch (opcode)
 	{
@@ -35,11 +36,34 @@ int extendedDecode(CPU& cpu, u8 opcode)
 
 int decode(CPU& cpu, u8 opcode)
 {
-	printf("Decoding %hhX\n", opcode);
+	//printf("Decoding %hhX\n", opcode);
 	switch (opcode)
 	{
-	case 0x20://JN RZ, n
-		printf("jump by %hhX\n", memc::fetchu8(cpu.memc, cpu.PC));
+	case 0x0C:// INC C
+		{
+			BITCLEAR(cpu.registers.F, SUBSTRACT_FLAG_BIT);
+			if (HALF_CARRY_TEST(cpu.registers.C, (u8)0x1) != 0)
+				BITSET(cpu.registers.F, HALF_CARRY_FLAG_BIT);
+			else
+				BITCLEAR(cpu.registers.F, HALF_CARRY_FLAG_BIT);
+
+			if (cpu.registers.C + (u8)0x1 == (u8)0x0)
+				BITSET(cpu.registers.F, ZERO_FLAG_BIT);
+			else
+				BITCLEAR(cpu.registers.F, ZERO_FLAG_BIT);
+
+			cpu.registers.C = cpu.registers.C + (u8)0x1;
+		}
+		break;
+	case 0x0E:
+		cpu.registers.C = memc::fetchu8(cpu.memc, cpu.PC);
+		cpu.PC += 1;
+		break;
+	case 0x20://JN RZ, n (n is signed)
+		if (BITTEST(cpu.registers.F, ZERO_FLAG_BIT) == 0)
+			cpu.PC += memc::fetchs8(cpu.memc, cpu.PC) + 1; //+1, because we didn't advance PC after fetching offset.
+		else
+			cpu.PC += 1;
 		break;
 	case 0x21:
 		{//LD HL,d16
@@ -59,12 +83,19 @@ int decode(CPU& cpu, u8 opcode)
 			cpu.registers.HL = cpu.registers.HL - 1;
 		}
 		break;
+	case 0x3E:
+		cpu.registers.A = memc::fetchu8(cpu.memc, cpu.PC);
+		cpu.PC += 1;
+		break;
 	case 0xAF: //XOR A
 		cpu.registers.A ^= cpu.registers.A;
 		break;
 	case 0xCB://extended opcode
 		cpu.PC++;
 		return extendedDecode(cpu, memc::fetchu8(cpu.memc, cpu.PC-1));
+		break;
+	case 0xE2:
+		memc::writeu8(cpu.memc, 0xff00 + cpu.registers.C, cpu.registers.A);
 		break;
 	default:
 		printf("unknown opcode %hhX\n", opcode);
